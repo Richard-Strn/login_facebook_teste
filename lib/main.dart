240c277c544b072d4ec2af5344b5b141091d7375
Richard-Strn <115495391+Richard-Strn@users.noreply.github.com>
2024-06-20 23:14:58 -0300

    tentativa .env

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:xml/xml.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
 
-void main() {
+void main() async {
+// Carregar variáveis do arquivo .env
+  await dotenv.load(fileName: ".env");
   runApp(MyApp());
 }
 
@@ -23,6 +31,41 @@ class _HomeScreenState extends State<HomeScreen> {
   Map<String, dynamic>? _userData;
   AccessToken? _accessToken;
   String _loginStatus = 'Logged out';
+  String _xmlContent = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _loadXml();
+  }
+
+  Future<void> _loadXml() async {
+    try {
+      // Carrega o arquivo XML
+      final xmlString = await rootBundle
+          .loadString('android/app/src/main/res/values/strings.xml');
+      String updatedXmlString = xmlString;
+      print("xmlAntes, \n${updatedXmlString}");
+
+      // Substitui as variáveis no conteúdo do XML
+      dotenv.env.forEach((key, value) {
+        updatedXmlString = updatedXmlString.replaceAll('{{$key}}', value);
+      });
+
+      // Atualiza xml
+      final document = XmlDocument.parse(updatedXmlString);
+
+      // Converte o XML em uma string formatada
+      setState(() {
+        _xmlContent = document.toXmlString(pretty: true);
+        print("xmlDepois,\n${_xmlContent}");
+      });
+    } catch (e) {
+      setState(() {
+        _xmlContent = 'Erro ao carregar o arquivo XML: $e';
+      });
+    }
+  }
 
   void _login() async {
     final LoginResult result = await FacebookAuth.instance.login(
